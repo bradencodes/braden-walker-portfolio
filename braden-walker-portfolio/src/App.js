f8335441b01d359340918aed80a918371d8b776c
Braden Walker <bradenw37@gmail.com>
2019-01-09 14:17:17 -0800

    scanimation test working

--- braden-walker-portfolio/src/App.js
+++ braden-walker-portfolio/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import bar from './resources/bar.svg';
+import heart from './resources/heartTest.png';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="bars" style={{backgroundImage: `url(${bar})`}} />
+
+        <div className="parallax">
+
+          <div className="parallax__layer parallax__layer--base">
+            <img className="heart" src={heart} />
+          </div>
+
+          <div className="parallax__layer parallax__layer--back">
+            <div className="bars" style={{backgroundImage: `url(${bar})`}} />
+          </div>
+        
+        </div>
+
       </div>
     );
   }
