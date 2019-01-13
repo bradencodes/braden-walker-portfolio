25dd4f489b973aeb291ac70b8a5a45baf22fe5d1
Braden Walker <bradenw37@gmail.com>
2019-01-13 11:27:07 -0800

    cutout test working well

--- braden-walker-portfolio/src/App.js
+++ braden-walker-portfolio/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import bar from './resources/bars2-8.png';
+import bar from './resources/bars45.png';
+// import bar from './resources/bars2-8.png';
 // import bar from './resources/bar2-8.svg';
 import me from './resources/me2-8.png';
 import about from './resources/about2-8.png';
 import projects from './resources/projects2-8.png';
 import articles from './resources/articles2-8.png';
 import resume from './resources/resume2-8.png';
-import contact from './resources/contact2-8.png';
+import contact from './resources/contact45.png';
 import './App.css';
 
 class App extends Component {
@@ -14,15 +15,25 @@ class App extends Component {
     return (
       <div className="App">
 
+
+        <div className="cutout-container">
+          <img className="parallax__layer parallax__layer--back scanimation" src={contact} int="contact" />
+        </div>
+
+
         <div className="scanimations">
-          <img className="parallax__layer parallax__layer--back scanimation" src={about} int="about" />
+          {/* <img className="parallax__layer parallax__layer--back scanimation" src={about} int="about" />
           <img className="parallax__layer parallax__layer--back scanimation" src={projects} int="projects" />
           <img className="parallax__layer parallax__layer--back scanimation" src={articles} int="articles" />
-          <img className="parallax__layer parallax__layer--back scanimation" src={resume} int="resume" />
+          <img className="parallax__layer parallax__layer--back scanimation" src={resume} int="resume" /> */}
           <img className="parallax__layer parallax__layer--back scanimation" src={contact} int="contact" />
         </div>
         
         <div className="foreground bars" style={{backgroundImage: `url(${bar})`}} />
+
+        <div className="bars-container">
+          <img src={bar} alt="bars" className="foreground bars" />
+        </div>
 
         <div className="scanimations">
           <img className="parallax__layer parallax__layer--me scanimation" src={me} int="about" />
