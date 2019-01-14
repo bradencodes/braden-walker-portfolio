46081b3326284ba632e1456b2317627d8f729bd7
Braden Walker <bradenw37@gmail.com>
2019-01-13 16:47:38 -0800

    clean up code and add image 45s

--- braden-walker-portfolio/src/App.js
+++ braden-walker-portfolio/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import bar from './resources/bars45large.png';
-// import bar from './resources/bars2-8.png';
-// import bar from './resources/bar2-8.svg';
-import me from './resources/me2-8.png';
-import about from './resources/about2-8.png';
-import projects from './resources/projects2-8.png';
-import articles from './resources/articles2-8.png';
-import resume from './resources/resume2-8.png';
+import me from './resources/me45.png';
+import about from './resources/about45.png';
+import projects from './resources/projects45.png';
+import articles from './resources/articles45.png';
+import resume from './resources/resume45.png';
 import contact from './resources/contact45.png';
 import './App.css';
 
@@ -15,26 +13,21 @@ class App extends Component {
     return (
       <div className="App">
 
-
         <div className="cutout-container">
           <img className="parallax__layer parallax__layer--back scanimation" src={contact} int="contact" />
         </div>
 
 
         <div className="scanimations">
-          {/* <img className="parallax__layer parallax__layer--back scanimation" src={about} int="about" />
+          <img className="parallax__layer parallax__layer--back scanimation" src={about} int="about" />
           <img className="parallax__layer parallax__layer--back scanimation" src={projects} int="projects" />
           <img className="parallax__layer parallax__layer--back scanimation" src={articles} int="articles" />
-          <img className="parallax__layer parallax__layer--back scanimation" src={resume} int="resume" /> */}
+          <img className="parallax__layer parallax__layer--back scanimation" src={resume} int="resume" />
           <img className="parallax__layer parallax__layer--back scanimation" src={contact} int="contact" />
-        </div>
-        
-        <div className="bars-container">
-          <div className="foreground bars" style={{backgroundImage: `url(${bar})`}} />
         </div>
 
         <div className="bars-container">
-          <img src={bar} alt="bars" className="foreground bars" />
+          <img src={bar} alt="bars" className="foreground" />
         </div>
 
         <div className="scanimations">
@@ -42,7 +35,7 @@ class App extends Component {
         </div>
         
         <div className="bars-container">
-          <img src={bar} alt="bars" className="foreground bars" />
+          <img src={bar} alt="bars" className="foreground" />
         </div>
         
         <div className="bottomWhite"></div>
