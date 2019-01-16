22b94b3d4941106c3be92a85890f174fc4eef7d7
Braden Walker <bradenw37@gmail.com>
2019-01-16 10:02:02 -0800

    fix bug with squished picture of me

--- braden-walker-portfolio/src/components/Projects.js
+++ braden-walker-portfolio/src/components/Projects.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import bars from '../resources/scanimation/bars45large.png';
 import magentaBars from '../resources/scanimation/magentaBars.png';
 import projects_scanimation from '../resources/scanimation/projects45.png';
+import { projectsArray } from '../resources/projectsArray.js';
 
 class Projects extends Component {
     render() {
@@ -32,6 +33,38 @@ class Projects extends Component {
                         </div>
 
                     </div>
+
+                </div>
+
+                <div className="projects-content-container">
+
+                    {projectsArray.map(project => (
+
+                        <div key={project.id} className="project-cutout">
+
+                            <img src={project.image} alt={project.title} />
+
+                            <div className="text">
+                                <div className="title">{project.title}</div>
+
+                                <div className="description">{project.description}</div>
+
+                                {project.tools.map((tool, index) => {
+                                    if (index === 0 && project.tools.length === 1) return (<div>made with <b>{tool}</b></div>);
+                                    else if (index === 0) return (<div>made with <b>{tool}</b>, </div>);
+                                    else if (index === project.tools.length-1) return (<div><b>{tool}</b></div>);
+                                    else return (<div><b>{tool}</b>, </div>)
+                                })}
+                            </div>
+
+                            <div className="links">
+                                <a className="github" href={project.github}>github</a>
+                                <a className="visit" href={project.visit}>visit</a>
+                            </div>
+
+                        </div>
+                        
+                    ))}
 
                 </div>
 
---
+++ braden-walker-portfolio/src/resources/projectsArray.js
@@ -0,0 +1,13 @@
+import cookbook from './project_images/cookbook.png';
+
+export const projectsArray = [
+    {
+        "id": 0,
+        "title": "COOKBOOK",
+        "description": "A website that allows you to save recipes from anywhere on the web into one place. You can view your recipes, schedule them in a calendar, and create a cohesive grocery list of all necessary ingredients.",
+        "image": cookbook,
+        "tools": ["React", "LESS", "Apollo Client", "GraphQL", "Prisma DB"],
+        "github": "https://github.com/Lambda-School-Labs/Labs8-Cookbook",
+        "visit": "https://www.your-cookbook.us",
+    },
+]
