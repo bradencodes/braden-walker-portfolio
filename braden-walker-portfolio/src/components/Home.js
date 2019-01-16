bae568eb3c082457bf04d36cd66c0afd2891430f
Braden Walker <bradenw37@gmail.com>
2019-01-16 08:16:32 -0800

    init projects component

--- braden-walker-portfolio/src/components/Home.js
+++ braden-walker-portfolio/src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Landing from "./Landing.js";
 import About from "./About.js";
+import Projects from "./Projects.js";
 
 class Home extends Component {
     render() {
@@ -11,6 +12,8 @@ class Home extends Component {
                 <Landing />
 
                 <About />
+
+                <Projects />
 
                 <Landing />
                 
---
+++ braden-walker-portfolio/src/components/Projects.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+
+import bars from '../resources/scanimation/bars45large.png';
+import magentaBars from '../resources/scanimation/magentaBars.png';
+import about from '../resources/scanimation/about45.png';
+import me_square from '../resources/icons/me-square.png';
+
+class Projects extends Component {
+    render() {
+        return (
+            <div className="about-container">
+
+                <div className="about-banner">
+
+                    <div className="bars-container-1">
+                        <img className="base" src={bars} alt="bars45large" />
+                    </div>
+
+                    <div className="cyan-bars-container">
+                        <img className="base" src={magentaBars} alt="magentaBars" />
+                    </div>
+
+                    <div className="bars-container-2">
+                        <img className="base" src={bars} alt="bars45large" />
+                    </div>
+
+                    <img className="parallax__layer--scanimation" src={about} alt="about" />
+
+                    <div className="about-sticker">
+
+                        <div className="parallax-layer--front">
+                            <div className="name">About</div>
+                        </div>
+
+                    </div>
+
+                </div>
+                
+                <div className="about-content-container">
+
+                    <div className="about-content-cutout">
+
+                        <img src={me_square} alt="braden" />
+
+                        <div className="about-and-skills">
+                            <div className="about-me">
+                                Hello, I'm <b>Braden Walker</b>. I'm a <b>full-stack web developer</b> who specializes 
+                                in <span>responsive design</span> and <span>single page applications</span>.
+
+                                I like to push myself by constantly creating new projects and learning new skills!
+                            </div>
+
+                            <div className="skills-container">
+                                <div className="skill">JavaScript</div>
+                                <div className="skill">React</div>
+                                <div className="skill">Redux</div>
+                                <div className="skill">HTML5</div>
+                                <div className="skill">CSS/LESS</div>
+                                <div className="skill">SQL/noSQL</div>
+                                <div className="skill">Python</div>
+                                <div className="skill">C</div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Projects;
