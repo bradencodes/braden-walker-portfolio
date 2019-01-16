8ce41a89ca0dab9c8ac968c8573d641199f18ef1
Braden Walker <bradenw37@gmail.com>
2019-01-16 10:18:59 -0800

    add dragscolling to desktop

--- braden-walker-portfolio/src/App.js
+++ braden-walker-portfolio/src/App.js
@@ -7,7 +7,7 @@ import Home from "./components/Home.js";
 class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className="app dragscroll">
 
         <Sidebar />
 
